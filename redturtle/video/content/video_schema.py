e984b693daf799a206c5f7c094bb9cfb7b966737
fdelia <fdelia@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-10-23 10:35:50 +0000

    tolto attributo Searchable

--- redturtle/video/content/video_schema.py
+++ redturtle/video/content/video_schema.py
@@ -4,14 +4,12 @@ from redturtle.video import videoMessageFactory as _
 VIDEO_SCHEMA=atapi.Schema((
         
     atapi.StringField('year',
-                searchable=True,
                 widget = atapi.StringWidget(
                     label = _(u'label_year', default=u'Year'),
                     description = '',
                     )),
 
     atapi.StringField('duration',
-                searchable=True,
                 widget = atapi.StringWidget(
                     label = _(u'label_duration', default=u'Duration'),
                     description = '',
