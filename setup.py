8d32f2c7ddd19464543a8f5362c5dea0c10c3b9b
keul <keul@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-02 17:12:45 +0000

    Version is 0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.2.3-unreleased'
+version = '0.3.0'
 
 tests_require=['zope.testing']
 
