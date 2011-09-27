29966c81a1e29f56f4fcb7853545cfc56345fd4e
keul <keul@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-09-27 16:16:40 +0000

    added a ``@@flowplayer-video-migration`` for migrate base Plone Flowplayer enabled contents to be transformed in RedTurtle Video contents

---
+++ redturtle/video/browser/migrateflowplayable.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from zope.interface import Interface
+from zope.formlib import form
+try: # >= 4.1
+    from five.formlib import formbase
+except ImportError: # < 4.1
+    from Products.Five.formlib import formbase
+from redturtle.video.migrator import migrateFlowplayerToRedTurtleVideo
+from Products.statusmessages.interfaces import IStatusMessage
+
+from redturtle.video import videoMessageFactory as _
+
+
+class IMigrateFlowplayerSchema(Interface):
+    pass
+
+
+class MigrateFlowplayerFile(formbase.PageForm):
+    form_fields = form.FormFields(IMigrateFlowplayerSchema)
+    label = u'Video files migration'
+    description = u'Migrate basic Flowplayer video files to RedTurtle Video'
+
+    @form.action(_(u'Migrate Flowplayer video files'))
+    def actionMigrate(self, action, data):
+        output = migrateFlowplayerToRedTurtleVideo(self.context)
+        if output:
+            IStatusMessage(self.request).addStatusMessage(output, type='info')
+        else:
+            IStatusMessage(self.request).addStatusMessage(_(u'Nothing to migrate'), type='info')
+        return self.request.response.redirect(self.context.absolute_url())
+
+    @form.action(_(u'Cancel'))
+    def actionCancel(self, action, data):
+        return self.request.response.redirect(self.context.absolute_url())
--- redturtle/video/migrator.py
+++ redturtle/video/migrator.py
@@ -1,7 +1,36 @@
 # -*- coding: utf-8 -*-
 
-from plone.app.blob.migrations import migrate
-
+from collective.flowplayer.interfaces import IVideo
+from collective.flowplayer.interfaces import IMediaInfo
 
 def migrateRTInternalVideo(context):
+    from plone.app.blob.migrations import migrate
     return migrate(context, 'RTInternalVideo')
+
+
+# helper to build custom blob migrators for the given type
+# some code stolen from the migration of plone.app.blob
+def makeMigrator(context):
+    """ generate a migrator for the given at-based portal type """
+    from Products.contentmigration.archetypes import InplaceATItemMigrator
+    
+    class FlowplayerMigrator(InplaceATItemMigrator):
+        src_portal_type = 'File'
+        src_meta_type = 'ATFile'
+        dst_meta_type = 'RTInternalVideo'
+        dst_portal_type = 'RTInternalVideo'
+
+        def last_migrate_mediaData(self):
+            media = IMediaInfo(self.old)
+            self.new.getField('width').set(self.new, media.width)
+            self.new.getField('height').set(self.new, media.height)
+
+    return FlowplayerMigrator
+
+def migrateFlowplayerToRedTurtleVideo(context):
+    from Products.contentmigration.walker import CustomQueryWalker
+    migrator = makeMigrator(context)
+    walker = CustomQueryWalker(context, migrator, use_savepoint=True, query={'object_provides': IVideo.__identifier__})
+    walker.go()
+    return walker.getOutput()
+
