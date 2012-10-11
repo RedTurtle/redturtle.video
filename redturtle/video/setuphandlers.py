06c8dd95eff47982495777828b9c4160c922e714
Luca Fabbri <luca@keul.it>
2012-10-11 18:03:58 +0200

    removed registration of *all* catalog indexes

--- redturtle/video/setuphandlers.py
+++ redturtle/video/setuphandlers.py
@@ -10,33 +10,6 @@ _PROPERTIES = [
     dict(name='default_videosize_h', type_='int', value=300),
 ]
 
-# form : http://maurits.vanrees.org/weblog/archive/2009/12/catalog
-def addKeyToCatalog(portal, logger=None):
-    '''Takes portal_catalog and adds a key to it
-    @param portal: context providing portal_catalog
-    '''
-    if logger is None:
-        # Called as upgrade step: define our own logger.
-        logger = logging.getLogger('redturtle.video')
-
-    catalog = getToolByName(portal, 'portal_catalog')
-    indexes = catalog.indexes()
-    # Specify the indexes you want, with ('index_name', 'index_type')
-    wanted = (('getYear', 'KeywordIndex'),
-              ('getDuration', 'KeywordIndex')
-              )
-
-    indexables = []
-    for name, meta_type in wanted:
-        if name not in indexes:
-            catalog.addIndex(name, meta_type)
-            indexables.append(name)
-            logger.info("Added %s for field %s.", meta_type, name)
-#    if len(indexables) > 0:
-#        logger.info("Indexing new indexes %s.", ', '.join(indexables))
-#        catalog.manage_reindexIndex(ids=indexables)
-
-
 def registerProperties(context, logger=None):
     ptool = getToolByName(context, 'portal_properties')
     props = ptool.redturtle_video_properties
@@ -52,7 +25,6 @@ def setupVarious(context):
 
     logger = context.getLogger('redturtle.video')
     portal = context.getSite()
-    addKeyToCatalog(portal, logger)
     registerProperties(portal, logger)
 
 
--- redturtle/video/tests/test_setup.py
+++ redturtle/video/tests/test_setup.py
@@ -14,12 +14,6 @@ class TestSetup(base.TestCase):
     def test_skin_installed(self):
         self.failUnless(self.qi.isProductInstalled('collective.flowplayer'))
 
-    def test_catalogindexes(self):
-        catalog_indexes = self.portal.portal_catalog.indexes()
-        self.failUnless("hasSplashScreenImage" in catalog_indexes)
-        self.failUnless("getYear" in catalog_indexes)
-        self.failUnless("getDuration" in catalog_indexes)
-
 
 def test_suite():
     suite = unittest.TestSuite()
